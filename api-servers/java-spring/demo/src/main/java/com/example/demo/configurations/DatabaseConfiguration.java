9967659999376ffba4d75e7fb80dff8a68899516
Mr.Amo-Addai <kwadwoamoad@gmail.com>
2024-10-11 08:50:04 +0000

    .

--- api-servers/java-spring/demo/src/main/java/com/example/demo/configurations/DatabaseConfiguration.java
+++ api-servers/java-spring/demo/src/main/java/com/example/demo/configurations/DatabaseConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 
@@ -17,10 +18,12 @@ public class DatabaseConfiguration {
 
     @Qualifier("jdbcProductService")
     @Autowired
+    @Lazy  // Lazy-load this dependency to break the circular reference
     JdbcTemplate jdbcProductTemplate;
 
     @Qualifier("jdbcUserService")
     @Autowired
+    @Lazy  // Lazy-load this dependency to break the circular reference
     JdbcTemplate jdbcUserTemplate;
 
     @Bean(name="dbProductService")
