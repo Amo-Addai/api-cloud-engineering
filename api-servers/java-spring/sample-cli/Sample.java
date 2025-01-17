641ba696a45099e3ff4594c13c12413bbaaebb82
Mr.Amo-Addai <kwadwoamoad@gmail.com>
2025-01-17 21:41:52 +0000

    .

---
+++ api-servers/java-spring/sample-cli/Sample.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Sample {
+
+    public static void main(String[] args) {
+        exec(new int[] {1,2,3,4});
+    }
+
+    public int exec(int[] nums) {
+        // TODO:
+    }
+
+}
+
+/*
+
+! QUESTION
+
+*/
