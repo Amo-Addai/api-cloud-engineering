f8e68c30be7c9a1e76a46da722a3ddcc84a8c09e
Mr.Amo-Addai <kwadwoamoad@gmail.com>
2025-10-08 21:19:27 +0000

    .

--- api-servers/node-nest/sample-app-1/src/alarms/n-tier/infrastructure/persistence/orm/repositories/alarm.repository.ts
+++ api-servers/node-nest/sample-app-1/src/alarms/n-tier/infrastructure/persistence/orm/repositories/alarm.repository.ts
@@ -5,6 +5,7 @@ import { Repository } from 'typeorm';
 import { AlarmsRepository } from '../../../../application/ports/alarms.repository';
 import { Alarm } from '../../../../domain/alarm';
 import { AlarmEntity } from '../entities/alarm.entity';
+import { AlarmMapper } from '../mappers/alarm.mapper';
 
 @Injectable()
 export class OrmAlarmRepository implements AlarmsRepository {
@@ -13,10 +14,25 @@ export class OrmAlarmRepository implements AlarmsRepository {
         private readonly alarmRepository: Repository<AlarmEntity>,
     ) {}
 
-    async findAll(): Promise<Alarm[]> {
+    async findAll_(): Promise<Alarm[]> {
         return this.alarmRepository.find();
     }
 
-    save: (Alarm) => Promise<Alarm> = (alarm: Alarm): Promise<Alarm> =>
+    async findAll(): Promise<Alarm[]> {
+        const entities = this.alarmRepository.find();
+        return entities.map(item => AlarmMapper.toDomain(item));
+    }
+
+    save_: (Alarm) => Promise<Alarm> = (alarm: Alarm): Promise<Alarm> =>
         this.alarmRepository.save(alarm)
+
+    save: (Alarm) => Promise<Alarm> = 
+        async (
+            alarm: Alarm,
+            [persistenceModel, newEntity]: any[] = []
+        ): Promise<Alarm> => (
+            persistenceModel = AlarmMapper.toPersistence(alarm),
+            newEntity = await this.alarmRepository.save(persistenceModel),
+            AlarmMapper.toDomain(newEntity)
+    )
 }
