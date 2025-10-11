abf2271d46edde64715fa3adec28bd3514c7f7ea
Mr.Amo-Addai <kwadwoamoad@gmail.com>
2025-10-11 14:32:55 +0000

    .

--- api-servers/node-nest/sample-app-1/src/alarms/n-tier/application/ports/alarms.repository.ts
+++ api-servers/node-nest/sample-app-1/src/alarms/n-tier/application/ports/alarms.repository.ts
@@ -4,3 +4,7 @@ export abstract class AlarmsRepository {
     abstract findAll(): Promise<Alarm[]>;
     abstract save(alarm: Alarm): Promise<Alarm>;
 }
+
+export abstract class CreateAlarmsRepository {
+    abstract save(alarm: Alarm): Promise<Alarm>;
+}
