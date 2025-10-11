4cda98efebab13995c8d4aa2a7eb98858553bbad
Mr.Amo-Addai <kwadwoamoad@gmail.com>
2025-10-11 14:40:09 +0000

    .

--- api-servers/node-nest/sample-app-1/src/alarms/n-tier/application/ports/create-alarm.repository.ts
+++ api-servers/node-nest/sample-app-1/src/alarms/n-tier/application/ports/create-alarm.repository.ts
@@ -1,6 +1,5 @@
 import { Alarm } from '../../domain/alarm'
 
-export abstract class AlarmsRepository {
-    abstract findAll(): Promise<Alarm[]>;
+export abstract class CreateAlarmsRepository {
     abstract save(alarm: Alarm): Promise<Alarm>;
 }
--- api-servers/node-nest/sample-app-1/src/alarms/n-tier/application/queries/get-alarms.query-handler.ts
+++ api-servers/node-nest/sample-app-1/src/alarms/n-tier/application/queries/get-alarms.query-handler.ts
@@ -1,14 +1,23 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { Alarm } from '../../domain/alarm';
 import { GetAlarmsQuery } from './get-alarms.query';
+import { AlarmsRepository } from '../ports/alarms.repository';
+import { FindAlarmsRepository } from '../ports/find-alarms.repository';
 
 @QueryHandler(GetAlarmsQuery)
 export class GetAlarmsQueryHandler implements IQueryHandler<GetAlarmsQuery, Alarm[]> {
 
-    constructor(private readonly alarmRepository: AlarmRepository) {}
+    constructor(
+        private readonly alarmsRepository: AlarmsRepository,
+        private readonly findAlarmsRepository: FindAlarmsRepository,
+    ) {}
 
     async execute(query: GetAlarmsQuery): Promise<Alarm[]> {
-        return this.alarmRepository.findAll();
+        return this.alarmsRepository.findAll();
+    }
+
+    async execute_(query: GetAlarmsQuery): Promise<AlarmReadModel[]> {
+        return this.findAlarmsRepository.findAll();
     }
 
 }
