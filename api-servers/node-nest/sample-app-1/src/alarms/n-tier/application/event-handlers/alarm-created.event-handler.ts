6ef25fdfa1d85bdc8474a4db8ccd98fd9788d5ed
Mr.Amo-Addai <kwadwoamoad@gmail.com>
2025-10-14 14:46:23 +0000

    .

--- api-servers/node-nest/sample-app-1/src/alarms/n-tier/application/event-handlers/alarm-created.event-handler.ts
+++ api-servers/node-nest/sample-app-1/src/alarms/n-tier/application/event-handlers/alarm-created.event-handler.ts
@@ -6,7 +6,26 @@ import { AlarmCreatedEvent } from '../../domain/events/alarm-created.event';
 export class AlarmCreatedEventHandler implements IEventHandler<AlarmCreatedEvent> {
     private readonly logger = new Logger(AlarmCreatedEventHandler.name);
 
-    handle(event: AlarmCreatedEvent) {
+    constructor(
+        private readonly upsertMaterializedAlarmRepository: UpsertMaterializedAlarmRepository,
+    ) {}
+
+    async handle(event: AlarmCreatedEvent) {
         this.logger.log(`Alarm created event: ${JSON.stringify(event)}`);
+
+        /**
+         * in a real world application,
+         * we would have to ensure that this operation is atomic
+         * in the read model (eg. because the data operation may fail)
+         * @for more info: Transactional inbox / outbox pattern
+         */
+        await this.upsertMaterializedAlarmRepository.upsert({
+            id: event.alarm.id,
+            name: event.alarm.name,
+            severity: event.alarm.severity.value,
+            triggeredAt: event.alarm.triggeredAt,
+            isAcknowledged: event.alarm.isAcknowledged,
+            items: event.alarm.items,
+        });
     }
 }
--- api-servers/node-nest/sample-app-1/src/alarms/n-tier/infrastructure/persistence/orm/orm-persistence.module.ts
+++ api-servers/node-nest/sample-app-1/src/alarms/n-tier/infrastructure/persistence/orm/orm-persistence.module.ts
@@ -1,19 +1,46 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AlarmsRepository } from '../../../application/ports/alarms.repository';
 import { AlarmEntity } from './entities/alarm.entity';
 import { AlarmItemEntity } from './entities/alarm-item.entity';
 import { OrmAlarmRepository } from './repositories/alarm.repository';
+import {
+    MaterializedAlarmView,
+    MaterializedAlarmViewSchema,
+} from './schemas/materialized-alarm-view.schema';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([AlarmEntity, AlarmItemEntity])],
+    imports: [
+        TypeOrmModule.forFeature([AlarmEntity, AlarmItemEntity]),
+        MongooseModule.forFeature([
+            { name: MaterializedAlarmView.name, schema: MaterializedAlarmViewSchema },
+        ])
+    ],
     providers: [
         {
             provide: AlarmsRepository,
             useClass: OrmAlarmRepository,
         },
+        {
+            provide: CreateAlarmsRepository,
+            useClass: OrmCreateAlarmRepository,
+        },
+        {
+            provide: FindAlarmsRepository,
+            useClass: OrmFindAlarmRepository,
+        },
+        {
+            provide: UpsertMaterializedAlarmsRepository,
+            useClass: OrmUpsertMaterializedAlarmRepository,
+        },
     ],
-    exports: [AlarmsRepository],
+    exports: [
+        AlarmsRepository,
+        CreateAlarmsRepository,
+        FindAlarmsRepository,
+        UpsertMaterializedAlarmsRepository,
+    ],
 })
 export class OrmAlarmPersistenceModule {}
